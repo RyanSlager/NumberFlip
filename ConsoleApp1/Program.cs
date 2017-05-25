4da8fe8a0912ac3df0bde5c7dab2776b45600ae2
danteCLRK <fryanslaucerers@gmail.com>
2017-05-25 03:22:27 -0400

    Number Flip
    This console application accepts a string of numbers from the user, and prints them back in reverse order.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // gets string of numbers from the user, assigns them to an array, and initializes an empty list object
+            Console.WriteLine("Please enter a string of numbers");
+            string numbersIn = Console.ReadLine();
+            var digits = numbersIn.ToArray();
+            var numbers = new List<int>();
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                // tries to parse chars into ints and sets a bool based on the result
+                bool result = Int32.TryParse(digits[i].ToString(), out int number);
+
+                // if the char was parsed into an int, the int is added to the numbers list, if not, a message is displayed
+                // to the user that the item was not a number, and will be dropped from the list
+                if (result)
+                {
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a number and has been dropped from the list.", digits[i]);
+                }
+
+            }
+
+            // converts the numbers list into an array and then reverses the array
+            int[] numArray = numbers.ToArray();
+            Array.Reverse(numArray);
+            
+            // prints the reversed numbers
+            for(int i = 0; i < numArray.Length; i++)
+            {
+                Console.Write(numArray[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
